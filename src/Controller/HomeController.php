b0dca6e46a8501f11daadf0de7cee2db6ba488ea
gaudetphilippe <gaudet.philippe@gmail.com>
2021-02-04 09:05:44 +0100

    fixtures done

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,7 +4,6 @@
 namespace App\Controller;
 
 
-use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
