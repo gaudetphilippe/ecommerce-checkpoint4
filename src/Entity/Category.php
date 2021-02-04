b37e5591a02f5111d8784fdf23fe04d44b717404
gaudetphilippe <gaudet.philippe@gmail.com>
2021-02-05 00:09:49 +0100

    pages product et category

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use App\Entity\Product;
 use App\Repository\CategoryRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
