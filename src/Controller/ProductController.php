af0ad3154492a1d1be8adc127322a5158041e810
gaudetphilippe <gaudet.philippe@gmail.com>
2021-02-09 10:59:43 +0100

    class form

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,14 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Entity\Product;
+use App\Form\ProductType;
 use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Generator\UrlGenerator;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class ProductController extends AbstractController
 {
@@ -50,6 +64,36 @@ class ProductController extends AbstractController
             'product' => $product,
             'urlGenerator' => $urlGenerator
         ]);
+
     }
+    /**
+     * @Route("/admin/product/create", name="product_create")
+     */
+    public function create(FormFactoryInterface $factory, Request $request,
+                           SluggerInterface $slugger, EntityManagerInterface $em)
+    {
+
+      $form = $this->createForm(ProductType::class);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()) {
+            $product = $form->getData();
+            $product->setSlug(strtolower($slugger->slug($product->getName())));
+
+            $em->persist($product);
+            $em->flush();
+            dd($product);
+        }
+
+        $formView = $form->createView();
+
+
+        return $this->render('product/create.html.twig', [
+            'formView' => $formView
+        ]);
+
+    }
+
 }
 
