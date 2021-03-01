645cdc2b71feb4679384f3b282bb95d6889c0550
gaudetphilippe <gaudet.philippe@gmail.com>
2021-03-01 20:38:56 +0100

    fonctionnalités avancées du composant form

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -84,6 +84,7 @@ class ProductController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted()){
+
             $em->flush();
 
             return $this->redirectToRoute('product_show', [
